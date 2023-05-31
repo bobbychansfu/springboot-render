50f2dcde9474eb1138d807c110df21791cc95c2b
bobbychansfu <bobbychansfu@gmail.com>
2023-05-30 21:25:55 -0700

    basic login - sessions

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.demo.models.User;
 import com.example.demo.models.UserRepository;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UsersController {  
@@ -32,6 +34,11 @@ public class UsersController {
         return "users/showAll";
     }
 
+    @GetMapping("/")
+    public RedirectView process(){
+        return new RedirectView("login");
+    }
+
     @PostMapping("/users/add")
     public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response){
         System.out.println("ADD user");
@@ -42,4 +49,41 @@ public class UsersController {
         response.setStatus(201);
         return "users/addedUser";
     }
+
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpServletRequest request, HttpSession session){
+        User user = (User) session.getAttribute("session_user");
+        if (user == null){
+            return "users/login";
+        }
+        else {
+            model.addAttribute("user",user);
+            return "users/protected";
+        }
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam Map<String,String> formData, Model model, HttpServletRequest request, HttpSession session){
+        // processing login
+        String name = formData.get("name");
+        String pwd = formData.get("password");
+        List<User> userlist = userRepo.findByNameAndPassword(name, pwd);
+        if (userlist.isEmpty()){
+            return "users/login";
+        }
+        else {
+            // success
+            User user = userlist.get(0);
+            request.getSession().setAttribute("session_user", user);
+            model.addAttribute("user", user);
+            return "users/protected";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String destroySession(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "/users/login";
+    }
+
 }
