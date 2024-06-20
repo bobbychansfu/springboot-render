daeb347171e38d58dd1cec3250a108f89f0678c7
bobbychansfu <bobbychansfu@gmail.com>
2024-06-20 11:46:42 -0700

    fix: edit blog post

--- src/main/java/com/example/demo/controllers/PostController.java
+++ src/main/java/com/example/demo/controllers/PostController.java
@@ -3,6 +3,9 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @Controller
@@ -10,6 +13,12 @@ public class PostController {
     @PostMapping("addPost")
     public String postMethodName(@RequestBody String entity) {
         System.out.println("adding post");
+        return entity;
+    }
+
+    @PutMapping("path/{id}")
+    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+        System.out.println("updating post");
         return entity;
     }
     
