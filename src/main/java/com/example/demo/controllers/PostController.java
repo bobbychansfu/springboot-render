27fb6de8458c36f7299b44a8b415065d5bb5760f
bobbychansfu <bobbychansfu@gmail.com>
2024-06-20 11:44:28 -0700

    feat: get blog posts

--- src/main/java/com/example/demo/controllers/PostController.java
+++ src/main/java/com/example/demo/controllers/PostController.java
@@ -3,6 +3,9 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -11,6 +14,11 @@ public class PostController {
     public String postMethodName(@RequestBody String entity) {
         System.out.println("adding post");
         return entity;
+    }
+    
+    @GetMapping("getPost")
+    public String getPost(@RequestParam String param) {
+        return new String();
     }
     
 }
