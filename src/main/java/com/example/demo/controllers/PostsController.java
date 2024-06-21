126af5c7f31432d5972e3e3122d4591397f5f830
bobbychansfu <bobbychansfu@gmail.com>
2024-06-20 18:14:25 -0700

    feat: adding blog post

---
+++ src/main/java/com/example/demo/controllers/PostsController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
+public class PostsController {
+    @PostMapping("addPost")
+    public String postMethodName(@RequestBody String entity) {
+        return entity;
+    }
+    
+}
