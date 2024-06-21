43422309082b31dde1e524761129e64951d50911
bobbychansfu <bobbychansfu@gmail.com>
2024-06-20 20:38:36 -0700

    feat: add blog post

--- src/main/java/com/example/demo/Helpers.java
+++ src/main/java/com/example/demo/Helpers.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import java.util.Date;
+
 public class Helpers {
     public static String getHello() {
         return "Hello";
+    }
+    public String formatDate(Date d) {
+        return d.toString();
     }
 }
