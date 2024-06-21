f749223070720076d75c9b691c55eea53f7e6100
bobbychansfu <bobbychansfu@gmail.com>
2024-06-20 19:09:08 -0700

    feat: edit blog post

--- src/main/java/com/example/demo/controllers/PostsController.java
+++ src/main/java/com/example/demo/controllers/PostsController.java
@@ -3,6 +3,9 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @Controller
@@ -12,5 +15,9 @@ public class PostsController {
         
         return entity;
     }
-    
+    @PutMapping("editPost/{id}")
+    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+        
+        return entity;
+    }
 }
