00c7ff8e5ef12a974a965030179acfce3ecb5357
bobbychansfu <bobbychansfu@gmail.com>
2024-06-20 18:20:24 -0700

    feat: edit post

--- src/main/java/com/example/demo/controllers/PostsController.java
+++ src/main/java/com/example/demo/controllers/PostsController.java
@@ -3,6 +3,9 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @Controller
@@ -11,5 +14,9 @@ public class PostsController {
     public String postMethodName(@RequestBody String entity) {
         return entity;
     }
-    
+    @PutMapping("editPost/{id}")
+    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+        
+        return entity;
+    }
 }
