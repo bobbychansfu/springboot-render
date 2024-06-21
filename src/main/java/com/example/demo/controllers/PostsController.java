f0c3ce8cfc135590b4f8658cbe096fd7d8a3c000
bobbychansfu <bobbychansfu@gmail.com>
2024-06-20 20:40:31 -0700

    Revert "feat: get blog post"
    This reverts commit bd72b2387baa1df2bdc4b8b75f46266dd490adee.

--- src/main/java/com/example/demo/controllers/PostsController.java
+++ src/main/java/com/example/demo/controllers/PostsController.java
@@ -3,9 +3,6 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @Controller
@@ -13,10 +10,6 @@ public class PostsController {
     @PostMapping("addPost")
     public String addPost(@RequestBody String entity) {
         return entity;
-    }
-    @GetMapping("path")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
     }
     
 }
